146a1a530b25174076994442b76da82e61011ecb
BarshaC <barsha.chaudhary@bison.howard.edu>
2022-05-23 14:16:39 -0400

    Update calculator.py
    Simple gui displays for the user to enter the numbers and do some simple calculation like addition,subtraction, division and multiplication

--- calculator.py
+++ calculator.py
@@ -1,7 +1,28 @@
 from tkinter import *
 import tkinter
 import tkinter.messagebox
-top = Tk()
+
+def process():
+    try:
+        number1 = Entry.get(E1)
+        number2 = Entry.get(E2)
+        operator = Entry.get(E3)
+        number1 = int(number1)
+        number2 = int(number2)
+        if operator == "+":
+            answer = number1 + number2
+        if operator == "-":
+            answer = number1 - number2
+        if operator == "*":
+            answer = number1 * number2
+        if operator == "/":
+            answer = number1/number2
+        Entry.insert(E4,0,answer)
+        print(answer)
+    except ValueError:
+        tkinter.messagebox.showwarning("Warning","Please enter the value in integer")
+    
+top = tkinter.Tk()
 L1 = Label(top,text='My calculator',).grid(row=0, column=1)
 L2 = Label(top, text='Number 1',).grid(row=1, column=0)
 L3 = Label(top, text='Number 2',).grid(row=2, column=0)
@@ -15,5 +36,5 @@ E3 = Entry(top, bd = 5)
 E3.grid(row = 3, column = 1)
 E4 = Entry(top, bd = 5)
 E4.grid(row=4, column=1)
-B= Button(top, text = "Submit",).grid(row=5, column=1,)
+B= Button(top, text = "Submit",command = process).grid(row=5, column=1,)
 top.mainloop()
